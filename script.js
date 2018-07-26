5701693d5c2b4c991722e45cabb248c6acb56c3c
SaenaiEU <41299605+SaenaiEU@users.noreply.github.com>
2018-07-26 22:53:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Joshua on 11/6/2016.
+ */
+
+var numStr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+function convert(input) {
+    return input.toUpperCase()
+        .replace(/\s/g, "   ")
+        .replace(/([A-Z])/g, ":F_$1:")
+        .replace(/([0-9])/g, function ($1) {
+            return ":" + numStr[$1] + ": ";
+        });
+}
+
+
+$(document).ready(function(){
+
+    $('#in').on('change keyup paste', function (e) {
+        $('#out').val(convert($("#in").val()));
+    });
+
+    $('#copy').click(function () {
+        $('#out').select();
+        document.execCommand('copy');
+    });
+
+});
